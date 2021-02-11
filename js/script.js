b7f39b394c035476df8978f924f993e5ea029714
konohovec <61086197+konohovec@users.noreply.github.com>
2021-02-11 17:17:47 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,19 +2,17 @@ function testWebP(callback) {
 
     var webP = new Image();
     webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
+    	callback(webP.height == 2);
     };
     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-    
-    testWebP(function (support) {
-    
+}
+testWebP(function (support) {
     if (support == true) {
-    document.querySelector('body').classList.add('webp');
-    }else{
-    document.querySelector('body').classList.add('no-webp');
-    }
-    });
+		document.querySelector('body').classList.add('webp');
+    } else {
+    	document.querySelector('body').classList.add('no-webp');
+    };
+});
 
 function testWebP(callback) {
     var webP = new Image();
@@ -29,7 +27,7 @@ testWebP(function (support) {
 	    document.querySelector('body').classList.add('webp');
     } else {
     	document.querySelector('body').classList.add('no-webp');
-    }
+    };
 });
 
 $(document).ready(function () {
